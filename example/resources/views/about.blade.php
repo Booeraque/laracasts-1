a0ed293a1155260ddd6f95969370125f36925803
s26904 <s26904@pjwstk.edu.pl>
2024-07-08 20:56:35 +0200

    End of day3

---
+++ example/resources/views/about.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <h1>Hello from the About Page</h1>
+    <p>The current weather is:</p>
+    <ul>
+            <li>Temperature: 75 degrees</li>
+            <li>Condition: Sunny</li>
+    </ul>
+</x-layout>
---
+++ example/resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <h1>Hello from the Contact Page</h1>
+    <p>Contact me by the following links:</p>
+    <ul>
+        <li><a href="https://twitter.com/yourtwitter">Twitter</a></li>
+        <li><a href="bengo.dev">My Blog</a></li>
+    </ul>
+</x-layout>
